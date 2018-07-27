2e01ca962532e79d2613c82bea0a295140aecce9
bugalot <jerome@taotesting.com>
2018-07-27 16:18:27 +0200

    Bump version 1.0.2

--- manifest.php
+++ manifest.php
@@ -23,7 +23,7 @@ return array(
     'label' => 'xmlEditRp',
     'description' => 'xml editing and debugging tools',
     'license' => 'GPL-2.0',
-    'version' => '1.0.1',
+    'version' => '1.0.2',
     'author' => 'Open Assessment Technologies SA',
     'requires' => array(
         'xmlEdit' => '>=1.1.0',
