c10d02d0f820e1fadfb52801840a4eba7c43bb33
Tamas Besenyei <tamas@besenyei.net>
2019-05-21 09:58:55 +0200

    update dependency version

--- manifest.php
+++ manifest.php
@@ -27,7 +27,8 @@ return array(
     'author' => 'Open Assessment Technologies SA',
     'requires' => array(
         'xmlEdit' => '>=1.1.0',
-        'taoQtiItem' => '>=2.27.0'
+        'taoQtiItem' => '>=20.1.0',
+        'tao' => '>=36.0.0'
     ),
     'acl' => array(
         array('grant', 'http://www.tao.lu/Ontologies/TAOItem.rdf#QTIManagerRole', array('ext'=>'xmlEditRp')),
