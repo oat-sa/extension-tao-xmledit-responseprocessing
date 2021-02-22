fd331774ef2c0b2a3ef20e814f461fc55c6594d6
Aleh Hutnikau <hutnikau@1pt.com>
2021-02-22 19:08:47 +0300

    chore!: move dependencies to composer.json

--- manifest.php
+++ manifest.php
@@ -23,13 +23,7 @@ return array(
     'label' => 'xmlEditRp',
     'description' => 'xml editing and debugging tools',
     'license' => 'GPL-2.0',
-    'version' => '1.2.0',
     'author' => 'Open Assessment Technologies SA',
-    'requires' => array(
-        'xmlEdit' => '>=1.1.0',
-        'taoQtiItem' => '>=20.1.0',
-        'tao' => '>=36.0.0'
-    ),
     'acl' => array(
         array('grant', 'http://www.tao.lu/Ontologies/TAOItem.rdf#QTIManagerRole', array('ext'=>'xmlEditRp')),
         array('grant', 'http://www.tao.lu/Ontologies/TAOItem.rdf#ItemsManagerRole', array('ext'=>'xmlEditRp'))
